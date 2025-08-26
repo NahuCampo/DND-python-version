b9a624d1df0839b19009062bd60a29f90628faf8
Ruben Nahuel Campo Ledesma <96516674+NahuCampo@users.noreply.github.com>
2025-08-26 21:02:38 +0000

    vista de cartas de personajes

--- app.py
+++ app.py
@@ -9,6 +9,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/personaje/crear')
+def crearPersonaje():
+    return render_template('personajes.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
